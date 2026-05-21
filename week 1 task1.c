ae7dca1d562ca5633ebdf20ebea881830521a5e1
PrinceMusyoka <princemusyoka4@gmail.com>
2026-05-21 08:11:51 -0700

    Add files via upload

---
+++ week 1 task1.c
@@ -0,0 +1,24 @@
+//program to display units offered this semester 
+
+/*
+Multiline comment
+Description : program to display units offered this semester
+Name:Prince Musyoka 
+Reg no:BDSA-03-0021/2026
+Date:16/05/2026
+Version 1
+*/
+#include<stdio.h> //pre-processor directive ,printf() ,scanf()
+
+int main(){
+	
+	//display units offered this semester
+	printf("structured programming!!\n");
+	printf("communication skills!!\n");
+	printf("health and wellness management!!\n");
+	printf("data ethics,privacy and governance!!\n");
+	printf("basic mathematics!!\n");
+	printf("digital literacy!!\n");
+	
+	return 0;
+}	
